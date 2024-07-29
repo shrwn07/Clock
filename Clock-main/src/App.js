274b40f8bd7b3d4d675573384c9249a71529601b
shrwn07 <shrwnkumar11@gmail.com>
2024-07-30 00:48:25 +0530

    push

---
+++ Clock-main/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AnalogClock from './Components/AnalogClock';
+
+function App() {
+  return (
+    <div className="App">
+      <AnalogClock />
+    </div>
+  );
+}
+
+export default App;
